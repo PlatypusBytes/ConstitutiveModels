e74f717216ef16197ed57081e538790337307168
aronnoordam <aronnoordam@gmail.com>
2025-04-15 15:40:42 +0200

    cleaned compile.bat

--- c_models/matsuoka_nakai/compile.bat
+++ c_models/matsuoka_nakai/compile.bat
@@ -1,13 +1,13 @@
 REM this script should be used in: Native Tools Command Prompt for VS 2022
 REM for x64 architecture, use x64 Native Tools Command Prompt for VS 2022
 
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-@REM cl /c /EHsc /W3 matsuoka_nakai.c /Fo:matsuoka_nakai.obj
-@REM link /DLL /OUT:matsuoka_nakai.dll matsuoka_nakai.obj /DEF:umat.def
+@REM call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
 @rem debug:
 @REM cl /c /EHsc /W3 /Zi /MD matsuoka_nakai.c ../utils.c ../stress_utils.c ../globals.c ../yield_surfaces/matsuoka_nakai_surface.c ../elastic_laws/hookes_law.c
 
 @rem release:
 cl /c /EHsc /W3 matsuoka_nakai.c ../utils.c ../stress_utils.c ../globals.c ../yield_surfaces/matsuoka_nakai_surface.c ../elastic_laws/hookes_law.c
+
+@rem link the dll
 link /DLL /OUT:matsuoka_nakai.dll matsuoka_nakai.obj utils.obj stress_utils.obj globals.obj matsuoka_nakai_surface.obj hookes_law.obj  /DEF:umat.def
